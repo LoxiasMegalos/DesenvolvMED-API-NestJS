1553b6f63791368fad1c5e03a9aa2bc0a98dc4b6
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-09-16 12:18:32 -0300

    Preparando repositório para deploy da API Projeto Integrador

---
+++ src/cadastro/controllers/cadastro.controller.ts
@@ -0,0 +1,58 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { CadastroService } from "../services/cadastro.service";
+import { DeleteResult } from "typeorm";
+import { Cadastro } from "../entities/cadastro.entity";
+import { Paciente } from "../../paciente/entities/paciente.entity";
+import { Medico } from "../../medico/entities/medico.entity";
+import { CadastroTemporarioDTO } from "../model/cadastrotemporariodto";
+import { Comentario } from "src/comentario/entities/comentario.entity";
+
+@Controller('/cadastro')
+export class CadastroController {
+
+    constructor(
+        private readonly service: CadastroService
+    ) { }
+
+    @Post('/medico')
+    @HttpCode(HttpStatus.CREATED)
+    createMedico(@Body() cadastroTemporarioDTO: CadastroTemporarioDTO): Promise<Medico> {
+        return this.service.createMedico(cadastroTemporarioDTO)
+    }
+
+    @Post('/paciente')
+    @HttpCode(HttpStatus.CREATED)
+    createPaciente(@Body() cadastroTemporarioDTO: CadastroTemporarioDTO): Promise<Paciente> {
+        return this.service.createPaciente(cadastroTemporarioDTO)
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+        return this.service.delete(id)
+    }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Cadastro[]> {
+        return this.service.findAll()
+    }
+
+    @Get('/comentarios/:id')
+    @HttpCode(HttpStatus.OK)
+    findComentariosByCadastroId(@Param('id', ParseIntPipe) id: number): Promise<Comentario[]> {
+        return this.service.findComentariosByCadastroId(id)
+    }
+
+    @Put('/medico')
+    @HttpCode(HttpStatus.OK)
+    updateMedico(@Body() cadastroTemporarioDTO: CadastroTemporarioDTO): Promise<Medico> {
+        return this.service.updateMedico(cadastroTemporarioDTO)
+    }
+
+    @Put('/paciente')
+    @HttpCode(HttpStatus.OK)
+    updatePaciente(@Body() cadastroTemporarioDTO: CadastroTemporarioDTO): Promise<Paciente> {
+        return this.service.updatePaciente(cadastroTemporarioDTO)
+    }
+}
